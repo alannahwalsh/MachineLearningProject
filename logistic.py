f8f5c453e32f4c3cad5cba20da9a247770e13a5e
Alannah Walsh <72142210+alannahwalsh@users.noreply.github.com>
2021-11-24 18:05:37 +0000

    Add files via upload

---
+++ logistic.py
@@ -0,0 +1,63 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#read in data
+df = pd.read_csv("Instagram_Data.csv")
+#Posts vs Followers
+X1=df.iloc[:,0]
+X2=df.iloc[:,1]
+X=np.column_stack((X1,X2))
+y=df.iloc[:,3]
+
+#Followers vs Following
+#X1=df.iloc[:,1]
+#X2=df.iloc[:,2]
+#X=np.column_stack((X1,X2))
+#y=df.iloc[:,3]
+
+#plt.scatter(X1[y=='M'],X2[y=='M'],color='blue',marker="o")
+#plt.scatter(X1[y=='F'],X2[y=='F'],color='red',marker="+")
+
+#plt.title('Plot of no. Posts vs no. Followers data on own')
+#plt.xlabel('no. Posts->')
+#plt.ylabel('no. Followers->')
+
+#plt.title('Plot of no. Followers vs no. Following data on own')
+#plt.xlabel('no. Followers->')
+#plt.ylabel('no. Following->')
+
+#plt.legend(["M","F"],loc='upper right',ncol=2,fontsize=8)
+#plt.show()
+
+#train model logistic regression
+model = LogisticRegression(penalty='none',solver='lbfgs').fit(X, y)
+print("Intercept %f"%(model.intercept_))
+print("Coefficients",model.coef_)
+print("Accuracy",model.score(X, y))
+predic = model.predict(X)
+
+#extract the slope to display from model coefs
+#line_bias = model.intercept_
+#line_w = model.coef_.T
+#points = [(line_w[0]*x+line_bias)/(-1*line_w[1])for x in X1]
+
+m = plt.scatter(X1[y=='M'],X2[y=='M'],color='blue',marker="o")
+f = plt.scatter(X1[y=='F'],X2[y=='F'],color='red',marker="o")
+
+m_pred = plt.scatter(X1[predic=='M'],X2[predic=='M'],color='yellow',marker='.') 
+f_pred = plt.scatter(X1[predic=='F'],X2[predic=='F'],color='green',marker='.')
+
+plt.rcParams['figure.constrained_layout.use'] = True 
+#decision_boundary = plt.plot(X1, points, color='black', linewidth=1) 
+plt.title('Logistic Regression of no. Posts vs no. Followers')
+#plt.title('Logistic Regression of no. Followers vs no. Following')
+plt.xlabel('no. Followers->')
+plt.ylabel('no. Following->')
+
+#plt.legend(["decision_boundary","m", "f", "m_pred", "f_pred"]
+plt.legend(["m", "f", "m_pred", "f_pred"]
+,loc='upper right',ncol=2,fontsize=7)
+
+plt.show()
---
+++ roc_curve.py
@@ -0,0 +1,80 @@
+from sklearn.metrics import roc_curve
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.dummy import DummyClassifier
+
+# Read in data
+df = pd.read_csv("Instagram_Data.csv")
+#Posts vs Followers
+#X1=df.iloc[:,0]
+#X2=df.iloc[:,1]
+#X=np.column_stack((X1,X2))
+#y=df.iloc[:,3]
+
+#Followers vs Following
+X1=df.iloc[:,1]
+X2=df.iloc[:,2]
+X=np.column_stack((X1,X2))
+y=df.iloc[:,3]
+
+# Compute polynomials
+poly = PolynomialFeatures(2)
+X_poly = poly.fit_transform(X)
+print("printing in order: tn, fp, fn, tp")
+# Train our models
+knnModel = KNeighborsClassifier(n_neighbors=1, weights='uniform').fit(X,y)
+knn_pred = knnModel.predict(X)
+tn,fp,fn,tp = confusion_matrix(y,knn_pred).ravel()
+#print("knn_model of no. Posts vs no. Followers:", tn, fp, fn, tp)
+print("knn_model of no. Followers vs no. Following", tn, fp, fn, tp)
+lgModel = LogisticRegression(C=8).fit(X_poly,y)
+lg_pred = lgModel.predict(X_poly)
+tn,fp,fn,tp = confusion_matrix(y, lg_pred).ravel()
+#print("logistic_regression_model of no. Posts vs no. Followers:", tn, fp, fn, tp)
+print("logistic_regression_model of no. Followers vs no. Following:", tn, fp, fn, tp)
+randomModel = DummyClassifier(strategy="uniform")
+mostFreqModel = DummyClassifier(strategy="most_frequent")
+randomModel.fit(X,y)
+mostFreqModel.fit(X,y)
+
+# Random
+rqndom_pred = randomModel.predict(X)
+tn, fp, fn, tp = confusion_matrix(y, rqndom_pred).ravel()
+#print("random_model of no. Posts vs no. Followers:", tn, fp, fn, tp)
+print("random_model of no. Followers vs no. Following:", tn, fp, fn, tp)
+# Uniform
+most_freq_pred = mostFreqModel.predict(X)
+tn, fp, fn, tp = confusion_matrix(y,most_freq_pred).ravel()
+#print("most_freq_value_model of no. Posts vs no. Followers:", tn, fp, fn, tp)
+print("most_freq_value_model of no. Followers vs no. Following:", tn, fp, fn, tp)
+
+# logistic roc
+fpr, tpr, _ = roc_curve(y, lgModel.decision_function(X_poly),pos_label='F')
+# knn roc
+knn_proba = knnModel.predict_proba(X)
+knn_fpr, knn_tpr, thresh = roc_curve(y, knn_proba[:,1],pos_label='F')
+# most freq val roc
+most_freq_proba = mostFreqModel.predict_proba(X)
+most_freq_fpr, most_freq_tpr, thresh = roc_curve(y,most_freq_proba[:,1],pos_label='F')
+# random roc
+rand_proba = randomModel.predict_proba(X)
+rand_fpr, rand_tpr, thresh = roc_curve(y,rand_proba[:,1],pos_label='F')
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(fpr, tpr, color='cyan')
+ax.plot(knn_fpr, knn_tpr, color='orange')
+ax.plot(most_freq_fpr, most_freq_tpr, color='blue')
+ax.plot(rand_fpr, rand_tpr, color='red')
+ax.plot([0,1], [0,1], color='green', linestyle='--')
+ax.set_ylabel('True positive rate',fontsize=10)
+ax.set_xlabel('False positive rate',fontsize=10)
+#ax.set_title("Cross-validation of neighbors of no. Posts vs no. Followers " + "range for a kNN model",fontsize=10)
+ax.set_title("Cross-validation of neighbors of no. Followers vs no. Following " + "range for a kNN model",fontsize=10)
+plt.legend(["Logistic Regression", "kNN", "Most Frequent value ", "Random"],fontsize=9)
+plt.show()
